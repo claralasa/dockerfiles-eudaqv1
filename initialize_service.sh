722dec0979e0914b91f57795acae9218582d68b4
Jordi Duarte Campderros <jorge.duarte.campderros@cern.ch>
2018-05-24 10:11:31 +0200

    Add privileged mode to TLU container and mechanism to deal with TLU device permissions inside

--- initialize_service.sh
+++ initialize_service.sh
@@ -18,7 +18,7 @@ then
 elif [ "X$1" == "XonlineMon" ];
 then
     sleep 10;
-    CMD="OnlineMon.exe -tc 0 -r tcp://172.20.128.2:44000";
+    CMD="OnlineMon.exe -tc 0 -sc 0 -r tcp://172.20.128.2:44000";
 elif [ "X$1" == "XTestProducer" ];
 then
     sleep 20;
@@ -29,6 +29,25 @@ then
     CMD="echo 'NOT IMPLEMENTED YET: NIProducer.exe -r tcp://172.20.128.2:44000'";
 elif [ "X$1" == "XTLU" ];
 then
+    # Deal with the permissions for the TLU
+    # 1. check if the tlunoroot utility is compiled
+    #    otherwise compile it
+    TLUCH=$(which tlunoroot)
+    if [ "X${TLUCH}" == "X" ]; 
+    then
+        sudo g++ -o /usr/bin/tlunoroot /eudaq/eudaq/producers/tlu/src/tlunoroot.cxx -lusb
+        TLUCH=$(which tlunoroot)
+        if [ "X${TLUCH}" == "X" ]; 
+        then
+            echo "ERROR: the utility tlunoroot NOT has been created!!"
+            exit -1;
+        fi;
+        # Change permissions
+        sudo chown root ${TLUCH}
+        sudo chmod u+s ${TLUCH}
+    fi;
+    # 2. Change permissions to the tlu
+    ${TLUCH}
     sleep 20;
     CMD="TLUProducer.exe -r tcp://172.20.128.2:44000";
 fi
